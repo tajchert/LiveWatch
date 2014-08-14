f2756742288e2d25cef8bda1c2b6dce2e9ff26e7
Michal Tajchert <mtajchert@gmail.com>
2014-08-14 15:33:04 +0200

    Dependency on accelerometer for generating background.

--- wear/src/main/java/pl/tajchert/livewatch/ActivityWatch.java
+++ wear/src/main/java/pl/tajchert/livewatch/ActivityWatch.java
@@ -22,12 +22,19 @@ public class ActivityWatch extends Activity  {
     private float mAccel; // acceleration apart from gravity
     private float mAccelCurrent; // current acceleration including gravity
     private float mAccelLast; // last acceleration including gravity
+    private int mAccelPrevious;
 
-    private static final int PROBABILITY = 5; //out of 10
-    private static final int SQUARE_SIZE = 10;
+    private static final double PROBABILITY = 0.3;
+    private static final int SQUARE_SIZE = 20;
     private static final int RES_SAMSUNG_GEAR_LIVE = 320;
     private ImageView backgroundView;
     private boolean mActive;
+
+    private Bitmap.Config conf;
+    private Bitmap bitmapLive;
+    private Canvas canvas;
+    private Paint p;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,8 @@ public class ActivityWatch extends Activity  {
                 backgroundView = (ImageView) stub.findViewById(R.id.liveBackground);
             }
         });
+        createBackground();
+
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
@@ -47,9 +56,21 @@ public class ActivityWatch extends Activity  {
         mAccelCurrent = SensorManager.GRAVITY_EARTH;
         mAccelLast = SensorManager.GRAVITY_EARTH;
     }
+
+    private void createBackground(){
+        conf = Bitmap.Config.ARGB_8888; // see other conf types
+        bitmapLive = Bitmap.createBitmap(RES_SAMSUNG_GEAR_LIVE, RES_SAMSUNG_GEAR_LIVE, conf); // this creates a MUTABLE bitmap
+        canvas = new Canvas(bitmapLive);
+        p = new Paint();
+        p.setColor(Color.GRAY);
+    }
+
     protected void onResume() {
         super.onResume();
         mActive = true;
+        if(bitmapLive == null){
+            createBackground();
+        }
         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
     protected void onPause() {
@@ -68,29 +89,49 @@ public class ActivityWatch extends Activity  {
             mAccelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
             float delta = mAccelCurrent - mAccelLast;
             mAccel = mAccel * 0.9f + delta; // perform low-cut filter
-            paintBackground();
+            if(bitmapLive != null && p!= null) {
+                paintBackground(mAccel);
+            }
         }
 
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     };
 
-    private void paintBackground(){
-        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
-        Bitmap bitmapLive = Bitmap.createBitmap(RES_SAMSUNG_GEAR_LIVE, RES_SAMSUNG_GEAR_LIVE, conf); // this creates a MUTABLE bitmap
-        Canvas canvas = new Canvas(bitmapLive);
-        Paint p = new Paint();
-        p.setColor(Color.WHITE);
+    private void paintBackground(float acceleration){
+        int accelerationAdd = (int) Math.abs(acceleration);
+        if(accelerationAdd - mAccelPrevious > 10){
+            //some wrong peaks - ignore
+            return;
+        }
+        if(accelerationAdd < 2){
+            canvas.drawColor(Color.BLACK);
+            backgroundView.setImageBitmap(bitmapLive);
+            return;
+        }
+        mAccelPrevious = accelerationAdd;
+        if(canvas == null){
+            createBackground();
+        }
+        accelerationAdd = normalizeAccelerometer(accelerationAdd);
+        canvas.drawColor(Color.BLACK);
         Random randomGenerator = new Random();
-
         for(int y = 0 ; y < RES_SAMSUNG_GEAR_LIVE ; y =y + SQUARE_SIZE){
             for(int x = 0 ; x < RES_SAMSUNG_GEAR_LIVE ; x = x + SQUARE_SIZE){
-                if(randomGenerator.nextInt(10) <= PROBABILITY) {
+                if(randomGenerator.nextInt(10) <= PROBABILITY * accelerationAdd) {
                     canvas.drawRect(x, y, (x + SQUARE_SIZE), (y + SQUARE_SIZE), p);
                 }
             }
         }
-
         backgroundView.setImageBitmap(bitmapLive);
+    }
+
+    private int normalizeAccelerometer(int accelerometer){
+        int newAcc;
+        if(accelerometer > 20){
+            return 10;
+        }
+        newAcc = (int) Math.round(accelerometer * 0.5);//to range 1-10
+        return newAcc;
     }
 }
